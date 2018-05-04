2b2d7d0c2d742141cba4823e3d2d64099cff3874
Ivan Dolgov <johndolgov@gmail.com>
2018-05-04 14:27:39 +0300

    new version

--- supertool/src/supertool/simfiles_finder.py
+++ supertool/src/supertool/simfiles_finder.py
@@ -1,16 +1,21 @@
 import os
-import sys
 import hashlib
 
 
-def chunk_reader(f_obj, chunk_size: int = 1024):
-    """
-    Generator that reads a file in bytes
+def printer(duplicates):
 
-    :param f_obj:
-    :param chunk_size:
-    :return:
-    """
+    if duplicates:
+        print('There is duplicate:')
+        for key, values in duplicates.items():
+            print('-------------------\n')
+            print('This files duplicate each other')
+            print('------------------\n')
+            print('\nand\n'.join(values))
+    else:
+        print('There is not duplicates here')
+
+
+def chunk_reader(f_obj, chunk_size: int = 1024):
     while True:
         chunk = f_obj.read(chunk_size)
         if not chunk:
@@ -18,67 +23,41 @@ def chunk_reader(f_obj, chunk_size: int = 1024):
         yield chunk
 
 
-def get_hash(filename, hash=hashlib.md5):
+def get_hash(filename, hash_func = hashlib.md5):
 
-    hashobj = hash()
-    with open(filename, 'rb') as file_obj:
-        for chunk in chunk_reader(file_obj):
-            hashobj.update(chunk)
-    return hashobj.hexdigest()
+    hashobj = hash_func()
+    file_object = open(filename, 'rb')
+    for chunk in chunk_reader(file_object):
+        hashobj.update(chunk)
+    hashed = hashobj.digest()
+    file_object.close()
+    return hashed
 
 
-def check_for_duplicates(paths, hash=hashlib.sha1):
-    hashes_by_size = {}
-    hashes_on_1k = {}
-    hashes_full = {}
+def check_for_duplicates(path):
 
-    for path in paths:
-        for dirpath, dirnames, filenames in os.walk(path):
-            for filename in filenames:
-                full_path = os.path.join(dirpath, filename)
-                try:
-                    file_size = os.path.getsize(full_path)
-                except (OSError,):
-                    pass
-
-                duplicate = hashes_by_size.get(file_size)
-
-                if duplicate:
-                    hashes_by_size[file_size].append(full_path)
-                else:
-                    hashes_by_size[file_size] = []  # create the list for this file size
-                    hashes_by_size[file_size].append(full_path)
-
-    # For all files with the same file size, get their hash on the 1st 1024 bytes
-    for __, files in hashes_by_size.items():
-        if len(files) < 2:
-            continue    # this file size is unique, no need to spend cpy cycles on it
-
-        for filename in files:
-            small_hash = get_hash(filename, first_chunk_only=True)
-
-            duplicate = hashes_on_1k.get(small_hash)
-            if duplicate:
-                hashes_on_1k[small_hash].append(filename)
+    hashes_size = {}
+    for dir_path, dir_names, file_names in os.walk(path):
+        for filename in file_names:
+            full_path = os.path.join(dir_path, filename)
+            if hashes_size.get(os.path.getsize(full_path)):
+                hashes_size[os.path.getsize(full_path)].append(full_path)
             else:
-                hashes_on_1k[small_hash] = []          # create the list for this 1k hash
-                hashes_on_1k[small_hash].append(filename)
+                hashes_size[os.path.getsize(full_path)] = []
+                hashes_size[os.path.getsize(full_path)].append(full_path)
 
-    # For all files with the hash on the 1st 1024 bytes, get their hash on the full file - collisions will be duplicates
-    for __, files in hashes_on_1k.items():
-        if len(files) < 2:
-            continue    # this hash of fist 1k file bytes is unique, no need to spend cpy cycles on it
+    if not os.path.exists(path):
+        raise ValueError("Directory do not exist")
 
+    result_size = list(filter(lambda entry: len(entry) > 1, hashes_size.values()))
+
+    hashes = {}
+    for files in result_size:
         for filename in files:
-            full_hash = get_hash(filename, first_chunk_only=False)
-
-            duplicate = hashes_full.get(full_hash)
-            if duplicate:
-                print("Duplicate found: %s and %s" % (filename, duplicate))
+            if hashes.get(get_hash(filename)):
+                hashes[get_hash(filename)].append(filename)
             else:
-                hashes_full[full_hash] = filename
-
-if sys.argv[1:]:
-    check_for_duplicates(sys.argv[1:])
-else:
-    print("Please pass the paths to check as parameters to the script")
+                hashes[get_hash(filename)] = []
+                hashes[get_hash(filename)].append(filename)
+    return dict(filter(lambda entry: len(entry[1]) > 1,
+                       hashes.items()))
