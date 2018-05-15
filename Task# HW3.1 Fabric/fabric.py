de4bcb1e5a226326cf8b334a1fd765ab9ba82efa
Ivan Dolgov <johndolgov@gmail.com>
2018-05-15 14:32:33 +0300

    Fabric added

--- Task# HW3.1 Fabric/fabric.py
+++ Task# HW3.1 Fabric/fabric.py
@@ -4,6 +4,22 @@ This module presents decorators fabric
 import functools
 
 
+def decorator_with_args(decorator):
+    """
+    Decorator which creates decorator with args
+    :param decorator:
+    :return:
+    """
+    @functools.wraps(decorator)
+    def wrapper(*args, **kwargs):
+        def inner_decorator(func):
+            result = decorator(func, *args, **kwargs)
+            functools.update_wrapper(result, func)
+            return result
+        return inner_decorator
+    return wrapper
+
+
 def on_of_switcher(func):
     """
     Function which switch on and off
@@ -23,34 +39,24 @@ def on_of_switcher(func):
     return func
 
 
-def decorator_with_args(decorator):
-    """
-
-    :param decorator:
-    :return:
-    """
-    @functools.wraps(decorator)
-    def wrapper(*args, **kwargs):
-        def inner_decorator(func):
-            result = decorator(func, *args, **kwargs)
-            functools.update_wrapper(result, func)
-            return result
-        return inner_decorator
-    return wrapper
-
-
 @on_of_switcher
 @decorator_with_args
 def fabric(decorator, lambda_function):
-    def lambda_function_mod(*args,**kwargs):
+    """
+    Creates decorator which modify a decorator from args
+    :param decorator: decorator which will be modified
+    :param lambda_function: lambda function which can modify decorator
+    :return: decorator which return modified decorator
+    """
+    def lambda_function_mod(*args, **kwargs):
         def inner_decorator(func):
             @functools.wraps(func)
             def inner_function(*args_2, **kwargs_2):
                 if fabric.is_on:
                     res = decorator(func, *args, **kwargs)
                 else:
-                    result = func
-                return lambda_function(result(*args_2, **kwargs_2))
+                    res = func
+                return lambda_function(res(*args_2, **kwargs_2))
             return inner_function
         return inner_decorator
     return lambda_function_mod
@@ -58,6 +64,12 @@ def fabric(decorator, lambda_function):
 
 @fabric(lambda x: x**2)
 def repeat(func, times):
+    """
+    This function repeat and calculate avarage value
+    :param func: function
+    :param times: qauntity of repeation times
+    :return: sum of decorator result
+    """
     @functools.wraps(func)
     def inner(*args, **kwargs):
         value = 0
@@ -66,3 +78,15 @@ def repeat(func, times):
         return value/times
     return inner
 
+
+@repeat(3)
+def spam(*args, **kwargs):
+    """Function for test"""
+    print("Spam here!")
+    return 3
+
+
+if __name__ == '__main__':
+    print(spam([1, 3, 5]))
+    fabric.off()
+    print(spam([1, 3, 5]))
